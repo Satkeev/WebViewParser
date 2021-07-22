a42601d874a8a22dc537c37853a299a43e70d5b6
Satkeev <kamit.satkeev@gmail.com>
2021-07-22 16:02:45 -0700

    first commit

---
+++ app/src/main/java/com/satkeev/github/webviewparser/MainActivity.java
@@ -0,0 +1,19 @@
+package com.satkeev.github.webviewparser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView)
+                 findViewById(R.id.webview);
+        webView.loadUrl("https://akipress.org/");
+    }
+}
